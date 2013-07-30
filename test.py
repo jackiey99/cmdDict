b2b3fc592dbe235bd3f04375e4fbd0ccac4b1a6a
andybaoxv <changyale123@gmail.com>
2013-07-30 18:28:03 -0400

    An Example

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
 """This is a test function
+Source code:
+http://stackoverflow.com/questions/2081586/web-scraping-with-python
 """
 
+import urllib2
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(urllib2.urlopen(\
+        'http://www.timeanddate.com/worldclock/astronomy.html?n=78').read())
+
+# Get Data and Sunrise data
+for row in soup('table',{'class':'spad'})[0].tbody('tr'):
+    tds = row('td')
+    print tds[0].string, tds[1].string
 
